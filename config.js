fdf59c3652b336b193dd10cf1766aeecc0adc9f1
GK-FY <166106253+GK-FY@users.noreply.github.com>
2024-04-07 09:38:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.email ="gk-fy@iname.com"
 global.location="Lahore,Pakistan."
 
 
-global.mongodb= process.env.MONGODB_URI || ""
+global.mongodb= process.env.MONGODB_URI || "mongodb+srv://darkdragonv4:ameesha@cluster0.ttcxkqt.mongodb.net"
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
